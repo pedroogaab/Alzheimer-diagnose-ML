631b7592122997695dd4f5845e1c74337d6ca2e3
pedroogaab <gdspedro.ml@gmail.com>
2025-05-25 17:10:41 -0300

    config api and structure data

--- api_metamodelo.py
+++ api_metamodelo.py
@@ -3,15 +3,20 @@ from pydantic import BaseModel
 import numpy as np
 import joblib
 
-scaler = joblib.load("transformers/scaler_alzheimer.pkl")
+scaler = joblib.load("transformers/scaler.pkl")
 label_encoder = joblib.load("transformers/label_encoders.pkl")
 model = joblib.load("metamodelo.pkl")
 
 api = FastAPI()
+
+
 class AlzheimerInput(BaseModel):
+    
+    
     AGE: float
     PTEDUCAT: int
     FDG: float
+    AV45: float
     CDRSB: float
     ADAS11: float
     ADAS13: float
@@ -25,6 +30,7 @@ class AlzheimerInput(BaseModel):
     MOCA: float
     EcogPtMem: float
     EcogPtLang: float
+    EcogPtVisspat: float
     EcogPtDivatt: float
     EcogPtTotal: float
     EcogSPMem: float
@@ -34,26 +40,36 @@ class AlzheimerInput(BaseModel):
     EcogSPOrgan: float
     EcogSPDivatt: float
     EcogSPTotal: float
-    FLDSTRENG: str
     FSVERSION: str
-    IMAGEUID: float
     Hippocampus: float
+    Entorhinal: float
+    ICV: float
     mPACCdigit: float
     mPACCtrailsB: float
     FAQTOTAL: float
-    ID_faq: float
-    ID_neurobat: float
     ADNI_MEM: float
+    PTGENDER: str
 
 
 app = FastAPI()
 
 @app.post("/predict")
 def predict(data: AlzheimerInput):
+    
+    ptgender = "Male" if data.PTGENDER == "Masculino" else "Female"
+    
+    ptgender_encoded = label_encoder["PTGENDER"].transform([ptgender])[0]
+    fsversion_encoded = label_encoder["FSVERSION"].transform([data.FSVERSION])[0]
+
+    categorical_encoded = np.array([[ptgender_encoded, fsversion_encoded]])
+        
+    
     input_data = np.array([[
+        
         data.AGE,
         data.PTEDUCAT,
         data.FDG,
+        data.AV45,
         data.CDRSB,
         data.ADAS11,
         data.ADAS13,
@@ -67,6 +83,7 @@ def predict(data: AlzheimerInput):
         data.MOCA,
         data.EcogPtMem,
         data.EcogPtLang,
+        data.EcogPtVisspat,
         data.EcogPtDivatt,
         data.EcogPtTotal,
         data.EcogSPMem,
@@ -76,22 +93,34 @@ def predict(data: AlzheimerInput):
         data.EcogSPOrgan,
         data.EcogSPDivatt,
         data.EcogSPTotal,
-        data.FLDSTRENG,
-        data.FSVERSION,
-        data.IMAGEUID,
         data.Hippocampus,
+        data.Entorhinal,
+        data.ICV,
         data.mPACCdigit,
         data.mPACCtrailsB,
         data.FAQTOTAL,
-        data.ID_faq,
-        data.ID_neurobat,
         data.ADNI_MEM,
     ]])        
     
+    input_data = np.concatenate((input_data, categorical_encoded), axis=1)  
+        
+    input_scaled = scaler.transform(input_data)
+    pred = model.predict(input_scaled)[0]
+
+    
     dx_mapping = {  
-        "CN":   "(CN) Cognitivamente Normal ",
-        "EMCI": "(EMCI) Comprometimento Cognitivo Leve - Precoce",
-        "LMCI": "(LMCI) Comprometimento Cognitivo Leve - Avançado ",
-        "SMC":  "(SMC) Comprometimento Segnificativo da Memória",
-        "AD":   "(AD) Doença de Alzheimer "
+        0:  "(CN) Cognitivamente Normal ",
+        1:  "(EMCI) Comprometimento Cognitivo Leve - Precoce",
+        2:  "(LMCI) Comprometimento Cognitivo Leve - Avançado ",
+        3:  "(SMC) Comprometimento Segnificativo da Memória",
+        4:  "(AD) Doença de Alzheimer "
     }
+    
+    diagnosis = dx_mapping.get(pred)
+    
+    
+    
+    
+    
+    
+    return {"diagnóstico_predito": diagnosis}
